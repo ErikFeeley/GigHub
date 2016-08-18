c98fcb571c9c4d0b7861c3187ad29cfe60415abf
ErikFeeley <ehfeeley@gmail.com>
2016-08-17 20:49:26 -0400

    had to quit... postman was not authenticating... cant find out why.... yet...

---
+++ GigHub/GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,33 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId) // web api by default does not look for this in the request body it expects them to be in url. so we have to decorate with from body
+        {
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- GigHub/GigHub/Models/ApplicationDbContext.cs
+++ GigHub/GigHub/Models/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace GigHub.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -16,6 +17,17 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig) // each attendance has a required gig
+                .WithMany() // reverse direction of relationship. from gig to attendances each gig can have many attendances... aka people attending the gig
+                .WillCascadeOnDelete(false); // disable cascade deletion to make sql server happy
+
+            // calling base here to make sure the identitydbcontext fluent api overrides are go.
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
