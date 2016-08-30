c7ab31fafed4792343806a435b7542d0867e1b48
Erik Feeley <efeeley@vectorform.com>
2016-08-30 16:12:34 -0400

    adding dtos!!! and an api for notifications

--- GigHub/GigHub/Models/Notification.cs
+++ GigHub/GigHub/Models/Notification.cs
@@ -12,7 +12,7 @@ namespace GigHub.Models
         public string OriginalVenue { get; private set; }
 
         [Required]
-        public Gig Gig { get; set; }
+        public Gig Gig { get; private set; }
 
         public Notification()
         {
