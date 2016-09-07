27ab36950d21b9addbd8ff69e8e45e5066d079f1
Erik Feeley <efeeley@vectorform.com>
2016-09-07 16:32:46 -0400

    more js refactoring module reveal pattern stuff

--- GigHub/GigHub/Scripts/app/app.js
+++ GigHub/GigHub/Scripts/app/app.js
@@ -1,34 +1,54 @@
-﻿
+﻿var AttendanceService = function () {
+  var createAttendance = function (gigId, done, fail) {
+    $.post('/api/attendances', { gigId: gigId })
+          .done(done)
+          .fail(fail);
+  };
 
-function initGigs() {
-    $('.js-toggle-attendance')
-                   .click(function (e) {
-                       var button = $(e.target);
-                       if (button.hasClass('btn-default')) {
-                           $.post('/api/attendances', { gigId: button.attr('data-gig-id') })
-                               .done(function () {
-                                   button
-                                       .removeClass('btn-default')
-                                       .addClass('btn-info')
-                                       .text('Going');
-                               })
-                               .fail(function () {
-                                   alert('Something failed!');
-                               });
-                       } else {
-                           $.ajax({
-                               url: '/api/attendances/' + button.attr('data-gig-id'),
-                               method: 'DELETE'
-                           })
-                           .done(function () {
-                               button
-                                   .removeClass('btn-info')
-                                   .addClass('btn-default')
-                                   .test('Going?');
-                           })
-                           .fail(function () {
-                               alert('Something failed!');
-                           });
-                       }
-                   });
-}
+  var deleteAttendance = function (gigId, done, fail) {
+    $.ajax({
+      url: '/api/attendances/' + gigId,
+      method: 'DELETE'
+    })
+     .done(done)
+     .fail(fail);
+  };
+
+  return {
+    createAttendance: createAttendance,
+    deleteAttendance: deleteAttendance
+  };
+}();
+
+var GigsController = function (attendanceService) {
+  var button;
+
+  var init = function () {
+    $('.js-toggle-attendance').click(toggleAttendance);
+  };
+
+  var toggleAttendance = function (e) {
+    button = $(e.target);
+
+    var gigId = button.attr('data-gig-id');
+
+    if (button.hasClass('btn-default'))
+      attendanceService.createAttendance(gigId, done, fail);
+    else
+      attendanceService.deleteAttendance(gigId, done, fail);
+  };
+
+  var done = function () {
+    var text = (button.text() == 'Going') ? 'Going?' : 'Going';
+
+    button.toggleClass('btn-info').toggleClass('btn-default').text(text);
+  };
+
+  var fail = function () {
+    alert('Something failed!');
+  };
+
+  return {
+    init: init
+  }
+}(AttendanceService); // immediate invoke
